80172f51b2068fc95eba8f539f574f5de94a6143
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-03-08 12:21:27 +0900

    add bin/autoload.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,8 @@
 <?php
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
 error_reporting(E_ALL);
 
-$loader = require dirname(__DIR__) . '/vendor/autoload.php';
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+require dirname(__DIR__) . '/bin/autoload.php';
 
 // set the resource client
 copy(dirname(__DIR__) . '/var/db/todo_test.dist.sqlite3', dirname(__DIR__) . '/var/db/todo_test.sqlite3');

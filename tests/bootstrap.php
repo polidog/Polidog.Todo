d1df7015d86ba2220a83a61e0bf738300faabb70
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-03-14 13:01:41 +0900

    fix comment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,7 @@
 <?php
-
 error_reporting(E_ALL);
 
 require dirname(__DIR__) . '/bin/autoload.php';
 
-// set the resource client
+// recover initial database
 copy(dirname(__DIR__) . '/var/db/todo_test.dist.sqlite3', dirname(__DIR__) . '/var/db/todo_test.sqlite3');

3d4da621ff58068b4a023165e7ebf4e8f561a9d9
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-04-01 17:12:08 +0900

    indent message

--- var/conf/messages.php
+++ var/conf/messages.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
-    'app_todo' => [true => 'Complete', false => 'Incomplete'],
+    'app_todo' => [
+        true => 'Complete',
+        false => 'Incomplete'
+    ],
     'form_todo' => [
         'submit_button' => 'Add',
         'required' => 'Required',

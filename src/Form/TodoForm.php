58d3cc3b42f7cfa872c3819e6d91d5d269914f4d
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-04-03 11:07:34 +0900

    code format

--- src/Form/TodoForm.php
+++ src/Form/TodoForm.php
@@ -31,14 +31,12 @@ class TodoForm extends AbstractForm
             'method' => 'post',
             'action' => '/',
         ]);
-
-        /** @var Tag $tag */
+        /* @var Tag $tag */
         $tag = $this->helper->get('tag');
         $form .= $tag('div', ['class' => 'form-group']);
         $form .= $this->input('title');
         $form .= $this->error('title');
         $form .= $this->helper->tag('/div') . PHP_EOL;
-
         // submit
         $form .= $this->input('submit');
         $form .= $this->helper->tag('/form');
@@ -58,15 +56,13 @@ class TodoForm extends AbstractForm
                 'class' => 'form-control',
                 'size' => 20
             ]);
-
         $this->setField('submit', 'submit')
             ->setAttribs([
                 'name' => 'submit',
                 'value' => $this->msg['submit_button'],
                 'class' => 'btn btn-primary'
             ]);
-
-        // validationの設定
+        // form validation
         $this->filter->validate('title')->is('strlenMin', 1);
         $this->filter->useFieldMessage('title', $this->msg['required']);
     }
